9bfbfc575bd22798bc136bda119e116b8f023961
Méline Wéry <Meline@dy174115.univ-rennes1.fr>
2016-11-16 09:55:06 +0100

    Version 1.1

--- script_cat_name.py
+++ script_cat_name.py
@@ -8,22 +8,78 @@ import re, nltk, glob
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 #nltk.download()
+
+
+
 stop_words = stopwords.words("english")
-list_name= []
 
+list_tag7 = [['NNP', 'NN', 'NN', '.', 'NN', 'NNP', 'CD']]
+list_tag6 = [['NNP', 'NNP', 'NN', 'NN', '.', 'NNP']]
+list_tag5 = [['NNP', 'NN', 'NNP', 'NN', 'NNP']]
+list_tag4 = [['NN', 'NN', 'NNP', 'PRP'], ['NNP', 'NN', 'NNP', 'CD'], ['NNP', 'NN', '.', 'NNP'], ['NNP', 'NN', '.', 'CC'], ['NNP', 'NNS', 'VBP', 'CD'], ['NNP', 'NNP', 'VBZ', 'CD'],['NNP', 'VBZ', 'NNP', 'CD']]
+list_tag3 = [['NNP', 'VBZ', 'NNP'], ['NNP', 'NN', 'CD'], ['NNP', 'NN', 'NNP'], ['NNP', 'VBD', 'CD'], ['NNP', 'NNS', 'NNP']]
+list_tag2 = [['NNP', 'NN'],['NNP', 'NNP'], ['NNP', 'NNS'], ['NNS', 'NNS'], ['NN', 'NN'], ['NNS', 'VBP']]
+list_tag1 = [['NNP'], ['NNS'], ['NN']]
 
-files=glob.glob(dirname+'*.txt')
-for file in files : 
-	with open(file, 'r') as f_in : 
-		for i, line in enumerate(f_in) :
-			if i==0 : 
-				list_taggs = []
-				tagg0, tagg1, tagg2, tagg3 = '','','',''
+list_tagg_v2 = ['NNP', 'NNS', 'NN']
+
+def main():
+	files=glob.glob(dirname+'*.txt')
+	for file in files : 
+		with open(file, 'r') as f_in : 
+			for n, line in enumerate(f_in) :
+				list_taggs= []
+				list_taggs2 = []
 				tokens = word_tokenize(line)
 				tokens = [i for i in tokens if i not in stop_words]
 				taggs = nltk.pos_tag(tokens)
-				for tagg in taggs :
-					list_taggs.append(tagg[1])
-					 
+				if n==0 : 
+					name=''
+					for tagg1 in taggs :
+						list_taggs.append(tagg1[1])
+					if check_tagg(list_taggs) : 
+						for tag in taggs :
+							name+=tag[0]+' '
+					if name!='' :
+				 		break
 
-				print list_taggs
+				nom=re.sub(r'(.+)([A-Z][a-z]+um) (.+)', r'\2', line)
+				print nom
+				#for tagg2 in taggs :
+				#	list_taggs2.append(tagg2[1])
+				#for x in list_tag2 : 
+				#	try :
+				#		print x
+				#		print list_taggs2
+				#		index= list_taggs2.index(','.join(x))
+				#		print index	
+				#	except : 
+				#		print "pas trouver"			
+					#list_name.append(tagg2[0])
+					
+				#print text_taggs2
+				#if list_taggs2 in list_tag2 : print list_tagg2
+					
+				
+
+
+
+
+def check_tagg(list):
+	if len(list)==1 : 
+		if list in list_tag1 : return True
+	if len(list)==2 :
+		if list in list_tag2 : return True
+	if len(list)==3 : 
+		if list in list_tag3 : return True
+	if len(list)==4 : 
+		if list in list_tag4 : return True
+	if len(list)==5 : 
+		if list in list_tag5 : return True
+	if len(list)==6 : 
+		if list in list_tag6 : return True
+	if len(list)==7 : 
+		if list in list_tag7 : return True
+
+if __name__ == '__main__':
+	main()
